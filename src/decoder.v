b4881fea137707f938ee3385839f785a30f2206e
d.mustakaev <dmutrymust@gmail.com>
2023-03-15 20:51:40 +0700

    Add bit reverse

--- src/decoder.v
+++ src/decoder.v
@@ -6,18 +6,18 @@ module decoder(
 
 	always @(posedge clk) 
 	begin
-		case(num)        //abcdefgh
-			4'd0: dlg = 8'b11111100;
-			4'd1: dlg = 8'b01100000;
-			4'd2: dlg = 8'b11011010;
-			4'd3: dlg = 8'b11110010;
+		case(num)        //hgfedcba
+			4'd0: dlg = 8'b00111111;
+			4'd1: dlg = 8'b00000110;
+			4'd2: dlg = 8'b01011011;
+			4'd3: dlg = 8'b01001111;
 			4'd4: dlg = 8'b01100110;
-			4'd5: dlg = 8'b10110110;
-			4'd6: dlg = 8'b10111110;
-			4'd7: dlg = 8'b11100000;
-			4'd8: dlg = 8'b11111100;
-			4'd9: dlg = 8'b11110100;
-			default: dlg = 8'b00000001;
+			4'd5: dlg = 8'b01101101;
+			4'd6: dlg = 8'b01111101;
+			4'd7: dlg = 8'b00000111;
+			4'd8: dlg = 8'b01111111;
+			4'd9: dlg = 8'b01101111;
+			default: dlg = 8'b10000000;
 		endcase
 	end
 endmodule
--- src/testbench.v
+++ src/testbench.v
@@ -10,7 +10,7 @@ module testbench();
 	reg [3:0]num = 4'd1;
 
 	wire a,b,c,d,e,f,g,h;
-	decoder decoder1(.clk(clk), .num(num), .dlg( {a,b,c,d,e,f,g,h} ));
+	decoder decoder1(.clk(clk), .num(num), .dlg( {h,g,f,e,d,c,b,a} ));
 
 	initial 
 	begin
